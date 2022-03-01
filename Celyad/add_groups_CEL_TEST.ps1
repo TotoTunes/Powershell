0857c478a066929135e91d14242ba0c5c95bd589
ThomasVdb94 <thomasvandenbossche94@gmail.com>
2022-03-01 22:21:25 +0100

    fixed copying groups in AAD

--- Celyad/add_groups_CEL_TEST.ps1
+++ Celyad/add_groups_CEL_TEST.ps1
@@ -7,23 +7,16 @@ $group_list = Get-AzureADUserMembership -ObjectId $AZ_Example_user.ObjectId
 
 foreach ($group in $group_list){
 $group.Mail
-$typ= Get-MsolGroup -SearchString $group.Mail
-$typ
+$typ= Get-MsolGroup -ObjectId $group.ObjectId
+Write-host $typ
 try {
-    Write-Host "adding Azure AD group" -ForegroundColor Yellow
     Add-AzureADGroupMember -ObjectID $group.objectId -RefObjectID $AZ_New_User.ObjectId -verbose
+    Write-Host "adding Azure AD group" -ForegroundColor Yellow
 }
 catch {
+    Add-DistributionGroupMember -Identity $group.Mail -Member $AZ_New_User.UserPrincipalName
     Write-Host "adding DL" -ForegroundColor Yellow
-    Add-DistributionGroupMember -Identity $group.DisplayName -Member $AZ_New_User.UserPrincipalName
-}
-finally {
-    Write-Host "adding Teams Group" -ForegroundColor Yellow
-    Add-UnifiedGroupLinks -Identity $group.Mail -LinkType "Members" -Links $AZ_New_User.Mail
-}
-
-
-Pause
+    }
 }
 
 #Get-AzureADUserMembership -ObjectId $user1ObjId |foreach { Add-AzureADGroupMember -ObjectId $_.ObjectId -RefObjectId $User2ObjId }
