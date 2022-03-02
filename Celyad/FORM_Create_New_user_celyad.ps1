f3cc8ff6ec11a905e837062ac0a4f20491e3963a
TotoTunes <thomasvandenbossche94@gmail.com>
2022-03-02 21:41:07 +0100

    comments and form creation

--- Celyad/FORM_Create_New_user_celyad.ps1
+++ Celyad/FORM_Create_New_user_celyad.ps1
@@ -1,7 +1,7 @@
 ﻿[void] [System.Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms")
 [void] [System.Reflection.Assembly]::LoadWithPartialName("System.Drawing")
 
-import-Module ActiveDirectory
+#import-Module ActiveDirectory
 
 #Function to connect to the O365 tennant
 function M365_Connection {
@@ -82,11 +82,70 @@ $Belgium = "OU=Celyad,OU=SBSUsers,OU=Users,OU=MyBusiness,DC=medpole,DC=local"
 $Domain = "medpole"
 $fileshare = "\\cel-fsv01\Users"
 
-Write-Host "Connecting to Office 365"
-M365_Connection
-Clear-Host
+#region form generation
+#begin to draw forms
+$Form = New-Object System.Windows.Forms.Form
+$Form.Text = "Computer Pinging Tool"
+$Form.Size = New-Object System.Drawing.Size(900, 900)
+$Form.StartPosition = "CenterScreen"
+
+$Form.KeyPreview = $True
+$Form.Add_KeyDown({ if ($_.KeyCode -eq "Enter")
+        { $x = $ListBox.SelectedItem; $Form.Close() } })
+$Form.Add_KeyDown({ if ($_.KeyCode -eq "Escape")
+        { $Form.Close() } })
+
+$label = New-Object System.Windows.Forms.Label
+$label.Location = New-Object System.Drawing.Size(5, 5)
+$label.Size = New-Object System.Drawing.Size(240, 30)
+$label.Text = "Type any computer name to test if it is on the network and can respond to ping"
+$Form.Controls.Add($label)
+
+$textbox = New-Object System.Windows.Forms.TextBox
+$textbox.Location = New-Object System.Drawing.Size(5, 40)
+$textbox.Size = New-Object System.Drawing.Size(120, 20)
+$textbox.Text = "Select source PC:"
+$Form.Controls.Add($textbox)
+ 
+$OKButton = New-Object System.Windows.Forms.Button
+$OKButton.Location = New-Object System.Drawing.Size(140, 38)
+$OKButton.Size = New-Object System.Drawing.Size(75, 23)
+$OKButton.Text = "OK"
+$OKButton.Add_Click($ping_computer_click)
+$Form.Controls.Add($OKButton)
+ 
+$result_label = New-Object System.Windows.Forms.label
+$result_label.Location = New-Object System.Drawing.Size(5, 65)
+$result_label.Size = New-Object System.Drawing.Size(240, 30)
+$result_label.Text = "Results will be listed here"
+$Form.Controls.Add($result_label)
+
+
+$ping_computer_click = { 
+    function pingTO {
+        $ComputerName = $textBox.Text.ToString()
+        if (Test-Connection $ComputerName -quiet -Count 2) {
+            Write-Host -ForegroundColor Green "Computer $ComputerName has network connection"
+        }
+        Else {
+            Write-Host -ForegroundColor Red "Computer $ComputerName does not have network connection"
+        }
+    }
+}
+
+#Show form
+$Form.Topmost = $True
+$Form.Add_Shown({ $Form.Activate() })
+[void] $Form.ShowDialog()
+#endregion
+
+#Write-Host "Connecting to Office 365"
+#M365_Connection
+#Clear-Host
 
 #parameters for user creation
+#region other code
+<#
 $firstname = Read-Host "Enter the first name"
 $Lastname = Read-Host "Enter the Last name"
 $login = Read-Host "Enther the username"
@@ -161,6 +220,10 @@ foreach ($group in $group_list) {
     }
 
 }
+#>
+
+
+#endregion
 
 <#
 $acl = get-acl -path $fileshare
