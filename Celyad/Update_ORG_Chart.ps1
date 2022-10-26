20d4f499d6a5daab8b9084e0e06493a2c715e68c
Thomas Vdb <thomasvandenbossche94@gmail.com>
2022-10-26 09:07:36 +0200

    Updated ORG chart

--- Celyad/Update_ORG_Chart.ps1
+++ Celyad/Update_ORG_Chart.ps1
@@ -1,3 +1,41 @@
 $import = Import-Csv -Path "C:\temp\202210-Employees-new-org-chart.csv" -Delimiter ";" -Header Name,Title,Department,Manager,Company
 
-$user = $import[1]
+foreach ($l in $import) {
+
+$user = $l
+$names = $user.Name.Split()
+$login = $names[0].Substring(0,1) + $names[1]
+Write-Host $login
+$AD= Get-ADUser -Identity $login
+
+if ($l.Manager.Length -eq 0)
+{
+   Set-ADUser -Identity $login -Clear Manager
+}
+else {
+$man = $user.Manager.Split()
+$manager = $man[0].Substring(0,1) + $man[1]
+  Set-ADUser -Identity $login -Manager $manager
+}
+
+if ($l.Department.Length -eq 0) 
+{
+     Set-ADUser -Identity $login -Clear Department
+}
+else
+{
+    Set-ADUser -Identity $login -Department $user.Department
+}
+
+if ($l.Title.Length -eq 0)
+{
+    Set-ADUser -Identity $login -Clear Title
+}
+else
+{
+    Set-ADUser -Identity $login -Title $user.Title 
+}
+
+Set-ADUser -Identity $login -Company $user.Company
+pause
+}
