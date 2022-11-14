bb12243b7c2dff8f7e11c171902d4843f42ed0aa
ThomasVdb94 <thomasvandenbossche94@gmail.com>
2022-11-14 15:05:06 +0100

    integrated out of office message

--- Celyad/Disable_user_Celyad.ps1
+++ Celyad/Disable_user_Celyad.ps1
@@ -12,6 +12,54 @@ function M365_Connection {
     Install-Module AzureAD
     Connect-AzureAd -Credential $LiveCred
 }
+#Function to generate text box and input out of office message
+function OutOfOfficeMessage {
+    Add-Type -AssemblyName System.Windows.Forms
+Add-Type -AssemblyName System.Drawing
+
+$form = New-Object System.Windows.Forms.Form
+$form.Text = 'Data Entry Form'
+$form.Size = New-Object System.Drawing.Size(550,650)
+$form.StartPosition = 'CenterScreen'
+
+$okButton = New-Object System.Windows.Forms.Button
+$okButton.Location = New-Object System.Drawing.Point(325,560)
+$okButton.Size = New-Object System.Drawing.Size(75,23)
+$okButton.Text = 'OK'
+$okButton.DialogResult = [System.Windows.Forms.DialogResult]::OK
+$form.AcceptButton = $okButton
+$form.Controls.Add($okButton)
+
+$cancelButton = New-Object System.Windows.Forms.Button
+$cancelButton.Location = New-Object System.Drawing.Point(400,560)
+$cancelButton.Size = New-Object System.Drawing.Size(75,23)
+$cancelButton.Text = 'Cancel'
+$cancelButton.DialogResult = [System.Windows.Forms.DialogResult]::Cancel
+$form.CancelButton = $cancelButton
+$form.Controls.Add($cancelButton)
+
+$label = New-Object System.Windows.Forms.Label
+$label.Location = New-Object System.Drawing.Point(10,20)
+$label.Size = New-Object System.Drawing.Size(280,20)
+$label.Text = 'Please enter the information in the space below:'
+$form.Controls.Add($label)
+
+$textBox = New-Object System.Windows.Forms.RichTextBox
+$textBox.Location = New-Object System.Drawing.Point(10,40)
+$textBox.Size = New-Object System.Drawing.Size(500,500)
+$form.Controls.Add($textBox)
+
+$form.Topmost = $true
+
+$form.Add_Shown({$textBox.Select()})
+$result = $form.ShowDialog()
+
+if ($result -eq [System.Windows.Forms.DialogResult]::OK)
+{
+    $x = $textBox.Text
+    Return $x
+}
+}
 
 $login = Read-Host "Enther the username"
 $AD = Get-aduser -identity $login
@@ -55,6 +103,6 @@ Set-Location "C:\temp"
 #convert to shared mailbox
 Get-mailbox -Identity "$login@celyad.com" | Set-MailBox -Type Shared
 #set out of office message
-
+$message = OutOfOfficeMessage
 
 #remove license
