4f03e7348767c3106fbb68171a6c760b865af229
Tototunes <thomasvandenbossche94@gmail.com>
2025-07-23 15:21:27 +0200

    updated input screen with 3rd text field for username

--- SimontBraun/SIB_NewUser.ps1
+++ SimontBraun/SIB_NewUser.ps1
@@ -142,6 +142,97 @@ function CreateFolder ($path, $username, $domain) {
     Write-Host "the folder $fullpath is created" -ForegroundColor Yellow
 }
 
+function Inputscreen{
+Add-Type -AssemblyName System.Windows.Forms
+Add-Type -AssemblyName System.Drawing
+
+# Create the main form
+$form = New-Object System.Windows.Forms.Form
+$form.Text = "User Input Form"
+$form.Size = New-Object System.Drawing.Size(600, 300) # Width, Height
+$form.StartPosition = "CenterScreen"
+$form.FormBorderStyle = [System.Windows.Forms.FormBorderStyle]::FixedSingle # Prevent resizing
+$form.MaximizeBox = $false # Disable maximize button
+$form.MinimizeBox = $false # Disable minimize button
+
+# Create a label for the first input field
+$label_firstname = New-Object System.Windows.Forms.Label
+$label_firstname.Text = "Enter the First name:"
+$label_firstname.Location = New-Object System.Drawing.Point(30, 30) # X, Y
+$label_firstname.AutoSize = $true # Adjust size to fit text
+
+# Create a text box for the first input field
+$text_firstname = New-Object System.Windows.Forms.TextBox
+$text_firstname.Location = New-Object System.Drawing.Point(150, 30)
+$text_firstname.Size = New-Object System.Drawing.Size(200, 20)
+$text_firstname.Anchor = [System.Windows.Forms.AnchorStyles]::Top -bor [System.Windows.Forms.AnchorStyles]::Left -bor [System.Windows.Forms.AnchorStyles]::Right
+
+# Create a label for the second input field
+$label_lastname = New-Object System.Windows.Forms.Label
+$label_lastname.Text = "Enter the last name:"
+$label_lastname.Location = New-Object System.Drawing.Point(30, 60)
+$label_lastname.AutoSize = $true
+
+# Create a text box for the second input field
+$text_lastname = New-Object System.Windows.Forms.TextBox
+$text_lastname.Location = New-Object System.Drawing.Point(150, 60)
+$text_lastname.Size = New-Object System.Drawing.Size(200, 20)
+$text_lastname.Anchor = [System.Windows.Forms.AnchorStyles]::Top -bor [System.Windows.Forms.AnchorStyles]::Left -bor [System.Windows.Forms.AnchorStyles]::Right
+
+# Create a label for the third input field
+$label_username = New-Object System.Windows.Forms.Label
+$label_username.Text = "Enter the username:"
+$label_username.Location = New-Object System.Drawing.Point(30, 90) # X, Y
+$label_username.AutoSize = $true # Adjust size to fit text
+
+# Create a text box for the third input field
+$text_username = New-Object System.Windows.Forms.TextBox
+$text_username.Location = New-Object System.Drawing.Point(150, 90)
+$text_username.Size = New-Object System.Drawing.Size(200, 20)
+$text_username.Anchor = [System.Windows.Forms.AnchorStyles]::Top -bor [System.Windows.Forms.AnchorStyles]::Left -bor [System.Windows.Forms.AnchorStyles]::Right
+
+# Create a check username button
+$CheckButton = New-Object System.Windows.Forms.Button
+$CheckButton.Text = "Check username"
+$CheckButton.Location = New-Object System.Drawing.Point(370, 90)
+$CheckButton.Size = New-Object System.Drawing.Size(100, 20)
+
+# define action for Check Username button
+    $CheckButton.Click {
+        $userlogin = $text_username.Text
+        CheckUsername($userlogin)
+
+    }
+
+# Create a submit button
+$submitButton = New-Object System.Windows.Forms.Button
+$submitButton.Text = "Submit"
+$submitButton.Location = New-Object System.Drawing.Point(150, 120)
+$submitButton.Size = New-Object System.Drawing.Size(100, 30)
+
+# Add controls to the form
+$form.Controls.Add($label_firstname)
+$form.Controls.Add($text_firstname)
+$form.Controls.Add($label_lastname)
+$form.Controls.Add($text_lastname)
+$form.Controls.Add($submitButton)
+$form.Controls.Add($label_username)
+$form.Controls.Add($text_username)
+$form.Controls.Add($CheckButton)
+
+# Set the form's accept button (allows pressing Enter to submit)
+$form.AcceptButton = $submitButton
+
+# Show the form
+$form.ShowDialog()
+
+# After the form is closed, you can access the values if needed (e.g., if you closed the form on submit)
+# For this example, the output is displayed within the form, so these lines are mostly for demonstration
+# $finalName = $textBox1.Text
+# $finalAge = $text_lastname.Text
+# Write-Host "Form closed. Final Name: $finalName, Final Age: $finalAge"
+}
+
 Write-Host "Connecting to Office 365"
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 
---
+++ SimontBraun/User-Input-Windows.ps1
@@ -0,0 +1,88 @@
+Add-Type -AssemblyName System.Windows.Forms
+Add-Type -AssemblyName System.Drawing
+
+# Create the main form
+$form = New-Object System.Windows.Forms.Form
+$form.Text = "User Input Form"
+$form.Size = New-Object System.Drawing.Size(600, 300) # Width, Height
+$form.StartPosition = "CenterScreen"
+$form.FormBorderStyle = [System.Windows.Forms.FormBorderStyle]::FixedSingle # Prevent resizing
+$form.MaximizeBox = $false # Disable maximize button
+$form.MinimizeBox = $false # Disable minimize button
+
+# Create a label for the first input field
+$label_firstname = New-Object System.Windows.Forms.Label
+$label_firstname.Text = "Enter the First name:"
+$label_firstname.Location = New-Object System.Drawing.Point(30, 30) # X, Y
+$label_firstname.AutoSize = $true # Adjust size to fit text
+
+# Create a text box for the first input field
+$text_firstname = New-Object System.Windows.Forms.TextBox
+$text_firstname.Location = New-Object System.Drawing.Point(150, 30)
+$text_firstname.Size = New-Object System.Drawing.Size(200, 20)
+$text_firstname.Anchor = [System.Windows.Forms.AnchorStyles]::Top -bor [System.Windows.Forms.AnchorStyles]::Left -bor [System.Windows.Forms.AnchorStyles]::Right
+
+# Create a label for the second input field
+$label_lastname = New-Object System.Windows.Forms.Label
+$label_lastname.Text = "Enter the last name:"
+$label_lastname.Location = New-Object System.Drawing.Point(30, 60)
+$label_lastname.AutoSize = $true
+
+# Create a text box for the second input field
+$text_lastname = New-Object System.Windows.Forms.TextBox
+$text_lastname.Location = New-Object System.Drawing.Point(150, 60)
+$text_lastname.Size = New-Object System.Drawing.Size(200, 20)
+$text_lastname.Anchor = [System.Windows.Forms.AnchorStyles]::Top -bor [System.Windows.Forms.AnchorStyles]::Left -bor [System.Windows.Forms.AnchorStyles]::Right
+
+# Create a label for the third input field
+$label_username = New-Object System.Windows.Forms.Label
+$label_username.Text = "Enter the username:"
+$label_username.Location = New-Object System.Drawing.Point(30, 90) # X, Y
+$label_username.AutoSize = $true # Adjust size to fit text
+
+# Create a text box for the third input field
+$text_username = New-Object System.Windows.Forms.TextBox
+$text_username.Location = New-Object System.Drawing.Point(150, 90)
+$text_username.Size = New-Object System.Drawing.Size(200, 20)
+$text_username.Anchor = [System.Windows.Forms.AnchorStyles]::Top -bor [System.Windows.Forms.AnchorStyles]::Left -bor [System.Windows.Forms.AnchorStyles]::Right
+
+# Create a check username button
+$CheckButton = New-Object System.Windows.Forms.Button
+$CheckButton.Text = "Check username"
+$CheckButton.Location = New-Object System.Drawing.Point(370, 90)
+$CheckButton.Size = New-Object System.Drawing.Size(100, 20)
+
+# define action for Check Username button
+$CheckButton.Click {
+$username = $text_username.Text
+CheckUsername($userlogin)
+
+}
+
+# Create a submit button
+$submitButton = New-Object System.Windows.Forms.Button
+$submitButton.Text = "Submit"
+$submitButton.Location = New-Object System.Drawing.Point(150, 120)
+$submitButton.Size = New-Object System.Drawing.Size(100, 30)
+
+# Add controls to the form
+$form.Controls.Add($label_firstname)
+$form.Controls.Add($text_firstname)
+$form.Controls.Add($label_lastname)
+$form.Controls.Add($text_lastname)
+$form.Controls.Add($submitButton)
+$form.Controls.Add($label_username)
+$form.Controls.Add($text_username)
+$form.Controls.Add($CheckButton)
+
+# Set the form's accept button (allows pressing Enter to submit)
+$form.AcceptButton = $submitButton
+
+# Show the form
+$form.ShowDialog()
+
+# After the form is closed, you can access the values if needed (e.g., if you closed the form on submit)
+# For this example, the output is displayed within the form, so these lines are mostly for demonstration
+# $finalName = $textBox1.Text
+# $finalAge = $text_lastname.Text
+# Write-Host "Form closed. Final Name: $finalName, Final Age: $finalAge"
